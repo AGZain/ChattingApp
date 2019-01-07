d1b27c23fe5ad0281fe2216d4cafe23c9c350370
AGZain <abdulzainulabedin@gmail.com>
2019-01-06 22:43:03 -0500

    authentication

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,10 +1,53 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 module.exports = function(app){
 
+  app.use(cookieParser());
+  app.use(session({
+    key:'user_login',
+    secret:'login_secret',
+    resave:false,
+    saveUninitialized:false,
+    cookie: {
+      expires:600000
+    }
+  }));
   app.get('/',(req,res) =>{
     res.render('index');
   });
-}
+
+  app.route('/login')
+    .get(function(req, res){
+    res.render('login');
+    });
+
+  
+  
+  // app.use(cookieParser());
+  // app.use(session({
+  //   key: 'user_login',
+  //   secret: 'login_secret',
+  //   resave: false,
+  //   saveUninitialized: false,
+  //   cookie: {
+  //     expres:60000
+  //   }
+  // }));
+  // app.use(function(req, res, next) {       // Catches access to all other pages
+  //   if(!req.session.accessToken) {       // requiring a valid access token
+  //       res.redirect('/login');
+  //   } else {
+  //       console.log("already logged in");
+  //       next();
+  //   }
+  // });
+
+//   app.use(session({
+//     secret: 'work hard',
+//     resave: true,
+//     saveUninitialized: false
+//   }));
+ }
